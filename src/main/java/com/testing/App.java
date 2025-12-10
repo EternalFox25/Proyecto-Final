6e18047b69a17c7958d0f74befad7988ef20f9df
Emilia <lopezemilifing@gmail.com>
2025-12-09 22:53:19 -0300

    main structure

--- src/main/java/com/testing/App.java
+++ src/main/java/com/testing/App.java
@@ -1,14 +1,38 @@
 package com.testing;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
+import com.testing.enums.MenuOption;
+
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "你好!" );
-        System.out.println( "这是Emilia Lopez的电脑， 欢迎你来到我们的计算机程序！" );
+    public static void main( String[] args ){
+        /* Linea para comprobar que el terminal puede mostrar caracteres chinos
+        System.out.println( "你好!\n这是Emilia Lopez的电脑， 欢迎你来到我们的计算机程序！" );
+        */
+        
+
+        Scanner scan = new Scanner(System.in); //Creamos el objeto Scanner
+        MenuOption choice;
+
+
+        do{
+            MenuOption.showMenu();
+            choice = MenuOption.fromOrdinal(Integer.parseInt(scan.nextLine()));//cambia el input del usuario a un int
+            
+            //switch para imprimir el menu enum
+            switch (choice) {
+                case RECOGNIZE:
+                        break;
+                    case WRITE:
+                        break;
+                    case SHOW:
+                        break;
+                    case EXIT:
+                        System.err.println("祝你一天愉快 (Que tengas un buen dia)");
+                        break;
+            }
+        }while(choice != MenuOption.EXIT);
+        scan.close();
 
     }//end static void main
 }//end App
---
+++ src/main/java/com/testing/enums/MenuOption.java
@@ -0,0 +1,37 @@
+package com.testing.enums;
+
+public enum MenuOption {
+        RECOGNIZE("Reconocer caracteres chinos"),
+        WRITE("Escribir caracteres chinos desde una descripción"), 
+        SHOW("Mostrar todos los caracteres en el programa y sus significados"),
+        EXIT("Salir del programa");
+
+        //declare var
+        private final String description;
+
+        //formato de MenuOption
+        MenuOption(String description){
+            this.description = description;
+        }//end MenuOption (constructor)
+
+        //Solo get, porque es una constante y no puede ser cambiado
+        public String getDescription(){
+            return description;
+        }
+
+        public static void showMenu(){//imprime el Menu
+            System.err.println("------- 汉宇次 (Caracteres chinos)  -------");//nombre del programa
+            for (MenuOption option : MenuOption.values()) {//for-each para mostrar el menu
+                System.err.println(option.ordinal()+1 +")" + option.getDescription());//imprime el numero de la opción y muestra la descripción 
+            }//end foreach
+            System.err.print("Elija (1 - " + MenuOption.values().length + "): ");//imprime para que el usuario sepa que hacer
+        }//end showMenu()
+
+        //cambia el input del usuario a una constante para el enum
+        public static MenuOption fromOrdinal(int choice){ 
+            if (choice < 1 || choice>values().length) {//if el usuario pone un input fuera del rango del enum
+                throw new IllegalArgumentException();//muestra un mensaje de error
+            }//end if
+            return values()[choice-1];//-1 para que el index sea correcto
+        }//end MenuOption fromOrdinal
+}//ends enum MenuOption
